5f48a1b95b4620c2a0341b9a0953c189cf2337b0
Peafield <peterjohncoles@gmail.com>
2023-05-22 13:34:12 +0100

    feat: server.go intialises data and is now able to send and recieve data

---
+++ server.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sort"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Score struct {
+	Player string `json:"player"`
+	Score  int    `json:"score"`
+	Time   int    `json:"time"`
+}
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	scores = make([]Score, 0)
+	mu     sync.Mutex
+)
+
+func main() {
+	loadInitialScores()
+	fmt.Println(scores)
+
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func loadInitialScores() {
+	data, err := ioutil.ReadFile("scores.json")
+	if err != nil {
+		fmt.Println("Error reading scores json file: ", err)
+		return
+	}
+	if err := json.Unmarshal(data, &scores); err != nil {
+		fmt.Println("Error unmarshalling scores file:", err)
+	}
+
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i].Score > scores[j].Score
+	})
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Uprader Error: ", err)
+		return
+	}
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		var newScore Score
+		if err := json.Unmarshal(p, &newScore); err != nil {
+			fmt.Println("Unmarshalling Error: ", err)
+			return
+		}
+
+		mu.Lock()
+		scores = append(scores, newScore)
+		sort.Slice(scores, func(i, j int) bool {
+			return scores[i].Score > scores[j].Score
+		})
+		mu.Unlock()
+
+		data, err := json.Marshal(scores)
+		if err != nil {
+			fmt.Println("Marshalling Error: ", err)
+			continue
+		}
+		if err := conn.WriteMessage(websocket.TextMessage, data); err != nil {
+			return
+		}
+	}
+}
